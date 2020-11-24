7d782d57a54bc57f1df43cc214d9099366675ce7
shavaiz-hussain <shavzzoc@gmail.com>
2020-11-24 08:24:20 +0500

    Manupilating the state

--- react-complete-guide/src/App.js
+++ react-complete-guide/src/App.js
@@ -17,10 +17,26 @@ class App extends Component {
             }
         ]
     }
+    switchNameHandler = () =>{
+        this.setState({
+            persons : [
+                {
+                    name: 'shavaiz',age:23
+                },
+                {
+                    name: 'Manu',age:29
+                },
+                {
+                    name: 'Maxcc',age:23
+                }
+            ]
+        })
+    }
   render() {
     return (
       <div className='App'>
            <h1>This is react app</h1>
+          <button onClick={this.switchNameHandler}>Switch Button</button>
             <Person name={this.state.persons[0].name} age={this.state.persons[0].age}/>
             <Person name={this.state.persons[1].name} age={this.state.persons[1].age}/>
             <Person name={this.state.persons[2].name} age={this.state.persons[2].age}>My Hobbies is racing</Person>
